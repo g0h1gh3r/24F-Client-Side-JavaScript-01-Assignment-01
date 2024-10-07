922668350ace42cb3d83538474f4dd118e4299f6
Henry Zeng <youhengzeng8@gmail.com>
2024-10-07 19:24:06 -0400

    finish the html and css and initialize my js file for the logic

---
+++ index.js
@@ -0,0 +1,22 @@
+const words = {
+  subject: ['The turkey', 'Mom', 'Dad', 'The dog', 'The elephant', 'The cat'],
+  verb: ['sat on', 'ate', 'danced with', 'saw', "doesn't like", 'kissed'],
+  adjective: ['a funny', 'a scary', 'a goofy', 'a slimy', 'a barking', 'a fat'],
+  object: ['goat', 'monkey', 'fish', 'cow', 'frog', 'bug', 'worm'],
+  location: [
+    'on the moon',
+    'on the chair',
+    'in my spaghetti',
+    'in my soup',
+    'on the grass',
+    'in my shoes',
+  ],
+}
+
+let currentIndices = {
+  subject: 0,
+  verb: 0,
+  adjective: 0,
+  object: 0,
+  location: 0,
+}
